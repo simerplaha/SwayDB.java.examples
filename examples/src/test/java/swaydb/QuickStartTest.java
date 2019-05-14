46adc508fdcba720ff3cbd1a4d03b18b176c9037
Valentyn Kolesnikov <javadev75@gmail.com>
2019-05-14 17:47:59 +0300

    Add sample application. #17

---
+++ examples/src/test/java/swaydb/QuickStartTest.java
@@ -0,0 +1,39 @@
+package quickstart;
+
+import java.util.AbstractMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class QuickStartTest {
+
+    @Test
+    public void quickStart() {
+        // Create a memory database
+        swaydb.java.Map<Integer, String> map = swaydb.java.memory.Map.create(Integer.class, String.class);
+
+        map.put(1, "one");
+        map.get(1);
+        map.remove(1);
+
+        // write 100 key-values atomically
+        map.put(IntStream.rangeClosed(1, 100)
+            .mapToObj(index -> new AbstractMap.SimpleEntry<>(index, String.valueOf(index)))
+            .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue())));
+
+        // Iteration: fetch all key-values withing range 10 to 90,
+        // update values and atomically write updated key-values
+/*
+  map
+    .from(10)
+    .takeWhile(_._1 <= 90)
+    .map {
+      case (key, value) =>
+        (key, value + "_updated")
+    }
+    .materialize
+    .flatMap(map.put)
+*/
+    }
+}
