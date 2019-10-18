6bf2cfafaa750bc5f651a0ffb25016e8038be417
simerplaha <simer.j@gmail.com>
2019-10-18 11:16:43 +1100

    update QuickStart with functions's example

--- src/main/java/quickstart/QuickStart.java
+++ src/main/java/quickstart/QuickStart.java
@@ -2,6 +2,7 @@ package quickstart;
 
 import swaydb.java.MapIO;
 import swaydb.java.PureFunction;
+import swaydb.java.Return;
 import swaydb.java.Stream;
 import swaydb.java.data.util.KeyVal;
 
@@ -13,16 +14,16 @@ class QuickStart {
 
   public static void main(String[] args) {
     //create a memory database.
-    MapIO<Integer, Integer, PureFunction.VoidM<Integer, Integer>> map =
+    MapIO<Integer, Integer, PureFunction<Integer, Integer, Return.Map<Integer>>> map =
       swaydb.java.memory.Map
-        .config(intSerializer(), intSerializer())
+        .configWithFunctions(intSerializer(), intSerializer())
         .init()
         .get();
 
-    //basic put and expire
-    map.put(1, 1, Duration.ofSeconds(1)).get();
-    map.get(1).get(); //get
-    map.remove(1).get(); //remove
+    map.put(1, 1).get(); //basic put
+    map.get(1).get(); //basic get
+    map.expire(1, Duration.ofSeconds(1)).get(); //basic expire
+    map.remove(1).get(); //basic remove
 
     //atomic write a Stream of key-value
     map.put(Stream.range(1, 100).map(KeyVal::create)).get();
@@ -31,12 +32,30 @@ class QuickStart {
     map
       .from(10)
       .takeWhile(keyVal -> keyVal.key() <= 90)
-      .map(keyVal -> KeyVal.create(keyVal.key(), keyVal.value() + 1000000))
+      .map(keyVal -> KeyVal.create(keyVal.key(), keyVal.value() + 5000000))
       .materialize()
       .flatMap(map::put)
       .get();
 
-    //print all key-values
+    //create a function that reads key & value and applies modifications
+    PureFunction.OnKeyValue<Integer, Integer, Return.Map<Integer>> function =
+      (key, value, deadline) -> {
+        if (key < 25) { //remove if key is less than 25
+          return Return.remove();
+        } else if (key < 50) { //expire after 2 seconds if key is less than 50
+          return Return.expire(Duration.ofSeconds(2));
+        } else if (key < 75) { //update if key is < 75.
+          return Return.update(value + 10000000);
+        } else { //else do nothing
+          return Return.nothing();
+        }
+      };
+
+    map.registerFunction(function); //register the function.
+
+    map.applyFunction(1, 100, function); //apply the function to all key-values ranging 1 to 100.
+
+    //print all key-values to view the update.
     map
       .forEach(System.out::println)
       .materialize()
