65f5006a1a97e35ad6ea1022bbd4dc8c8d784498
simerplaha <simer.j@gmail.com>
2020-03-03 15:16:43 +1100

    ReverseOrderingTest

---
+++ src/main/java/ordering/ReverseOrderingTest.java
@@ -0,0 +1,44 @@
+package ordering;
+
+
+import org.junit.jupiter.api.Test;
+import swaydb.java.Map;
+import swaydb.java.memory.MapConfig;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static swaydb.java.serializers.Default.intSerializer;
+
+class ReverseOrderingTest {
+
+  @Test
+  void reverse() {
+    Map<Integer, Integer, Void> map =
+      MapConfig.functionsOff(intSerializer(), intSerializer())
+        //provide a typed comparator that reverses ordering
+        .setTypedComparator((key1, key2) -> key1.compareTo(key2) * -1)
+        .get();
+
+    IntStream
+      .rangeClosed(1, 100)
+      .forEach(integer -> map.put(integer, integer));
+
+    List<Integer> actual =
+      map
+        .keys()
+        .stream()
+        .materialize();
+
+    List<Integer> expected =
+      IntStream
+        .rangeClosed(1, 100)
+        .map(i -> 100 - i + 1)
+        .boxed()
+        .collect(Collectors.toList());
+
+    assertEquals(expected, actual);
+  }
+}
