95c4c556d79ca7aab128c4325d3dabc991957680
simerplaha <simer.j@gmail.com>
2020-03-19 04:00:02 +1100

    bump to 0.13.3

--- src/main/java/configurations/ConfiguringLevels.java
+++ src/main/java/configurations/ConfiguringLevels.java
@@ -3,7 +3,6 @@ package configurations;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.duration.Duration;
 import scala.concurrent.duration.FiniteDuration;
-import scala.jdk.javaapi.CollectionConverters;
 import swaydb.Compression;
 import swaydb.Pair;
 import swaydb.configs.level.SingleThreadFactory;
@@ -24,6 +23,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+
+import static swaydb.java.StorageUnits.gb;
+import static swaydb.java.StorageUnits.mb;
 
 /**
  * This API requires improvement to suit Java. Currently
@@ -49,15 +51,137 @@ public class ConfiguringLevels {
         }
       };
 
+    //sample configuration for a memory level
+    MemoryLevelConfig memoryLevel =
+      MemoryLevelConfig
+        .builder()
+        .withMinSegmentSize(mb(4)) //4.mb
+        .withMaxKeyValuesPerSegment(100000)
+        .withCopyForward(false)
+        .withDeleteSegmentsEventually(true)
+        //todo provide easier way to access Shared from Java
+        .withCompactionExecutionContext(CompactionExecutionContext.shared())
+        .withThrottle(
+          (LevelMeter levelMeter) -> {
+            if (levelMeter.levelSize() > gb(1)) {
+              return new Throttle(Duration.Zero(), 10);
+            } else {
+              return new Throttle(Duration.Zero(), 0);
+            }
+          }
+        )
+        .build();
+
+    //sample configuration for a persistent memory level
+    PersistentLevelConfig persistentLevel =
+      PersistentLevelConfig
+        .builder()
+        .withDir(Paths.get("level2"))
+        //todo provide simpler API for Java
+        .withOtherDirs(Arrays.asList(new Dir(Paths.get("level2-1"), 1), new Dir(Paths.get("level2-3"), 1)))
+        .withMmapAppendix(true)
+        .withAppendixFlushCheckpointSize(mb(4)) //4.mb
+        .withSortedKeyIndex(
+          SortedKeyIndex
+            .builder()
+            .withPrefixCompression(new PrefixCompression.Disable(true))
+            .withEnablePositionIndex(true)
+            .withIoStrategy(ioAction -> new IOStrategy.SynchronisedIO(true))
+            .withCompressions(info -> Collections.emptyList())
+            .build()
+        )
+        .withRandomKeyIndex(
+          RandomKeyIndex
+            .builder()
+            .withMaxProbe(1)
+            .withMinimumNumberOfKeys(5)
+            .withMinimumNumberOfHits(2)
+            .withIndexFormat(IndexFormat.Reference$.MODULE$)
+            .withAllocateSpace(RandomKeyIndex.RequiredSpace::requiredSpace)
+            .withIoStrategy(ioAction -> new IOStrategy.SynchronisedIO(true))
+            .withCompression(info -> Collections.emptyList())
+            .build()
+        )
+        .withBinarySearchIndex(
+          BinarySearchIndex.fullIndexBuilder()
+            .withMinimumNumberOfKeys(10)
+            .withIoStrategy(ioAction -> new IOStrategy.SynchronisedIO(true))
+            .withIndexFormat(IndexFormat.copyKey())
+            .withSearchSortedIndexDirectly(true)
+            .withCompression(info -> Collections.emptyList())
+            .build()
+        )
+        .withMightContainKeyIndex(
+          MightContainIndex.builder()
+            .withFalsePositiveRate(0.01)
+            .withUpdateMaxProbe(optimalMaxProbe -> 1)
+            .withMinimumNumberOfKeys(10)
+            .withIoStrategy(ioAction -> new IOStrategy.SynchronisedIO(true))
+            .withCompression(info -> Collections.emptyList())
+            .build()
+        )
+        .withValuesConfig(
+          ValuesConfig.builder()
+            .withCompressDuplicateValues(true)
+            .withCompressDuplicateRangeValues(true)
+            .withIoStrategy(ioAction -> new IOStrategy.SynchronisedIO(true))
+            .withCompression(info -> Collections.emptyList())
+            .build()
+        )
+        .withSegmentConfig(
+          SegmentConfig.builder()
+            .withCacheSegmentBlocksOnCreate(true)
+            .withDeleteSegmentsEventually(true)
+            .withPushForward(true)
+            .withMmap(MMAP.disabled())
+            .withMinSegmentSize(mb(4))
+            .withMaxKeyValuesPerSegment(100000)
+            .withIoStrategy(
+              ioStrategy -> {
+                if (ioStrategy.isOpenResource()) {
+                  return new IOStrategy.SynchronisedIO(true);
+                } else if (ioStrategy.isReadDataOverview()) {
+                  return new IOStrategy.SynchronisedIO(true);
+                } else {
+                  IOAction.DataAction action = (IOAction.DataAction) ioStrategy;
+                  return new IOStrategy.SynchronisedIO(action.isCompressed());
+                }
+              }
+            )
+            .withCompression(
+              info ->
+                Arrays.asList(
+                  Compression.lz4Pair(
+                    new Pair(LZ4Instance.fastestJavaInstance(), new LZ4Compressor.Fast(20.0)),
+                    new Pair(LZ4Instance.fastestJavaInstance(), LZ4Decompressor.fastDecompressor())
+                  ),
+                  new Compression.Snappy(20.0),
+                  Compression.noneCompression()
+                )
+            )
+            .build()
+        )
+        .withCompactionExecutionContext(CompactionExecutionContext.shared())
+        .withThrottle(
+          (LevelMeter levelMeter) -> {
+            FiniteDuration delay = new FiniteDuration(5 - levelMeter.segmentsCount(), TimeUnit.SECONDS);
+            int batch = Math.min(levelMeter.segmentsCount(), 5);
+            return new Throttle(delay, batch);
+          }
+        )
+        .build();
+
+    //create custom level hierarchy.
     SwayDBPersistentConfig config =
       ConfigWizard
-        .addPersistentLevel0( //level0
-          Paths.get("myMap"),
-          4000000, //4.mb
-          true,
-          RecoveryMode.reportFailure(),
-          new CompactionExecutionContext.Create(myTestSingleThreadExecutionContext),
-          Accelerator::cruise,
+        .withPersistentLevel0() //LEVEL 0
+        .withDir(Paths.get("myMap"))
+        .withMapSize(mb(4)) //4.mb
+        .withMmap(true)
+        .withRecoveryMode(RecoveryMode.reportFailure())
+        .withCompactionExecutionContext(new CompactionExecutionContext.Create(myTestSingleThreadExecutionContext))
+        .withAcceleration(Accelerator::cruise)
+        .withThrottle(
           (LevelZeroMeter meter) -> {
             int mapsCount = meter.mapsCount();
             if (mapsCount > 3) {
@@ -69,96 +193,9 @@ public class ConfiguringLevels {
             }
           }
         )
-        .addMemoryLevel1( //level1
-          4000000, //4.mb
-          100000,
-          false,
-          true,
-          //todo provide easier way to access Shared from Java
-          CompactionExecutionContext.Shared$.MODULE$,
-          (LevelMeter levelMeter) -> {
-            if (levelMeter.levelSize() > 1000000000) {
-              return new Throttle(Duration.Zero(), 10);
-            } else {
-              return new Throttle(Duration.Zero(), 0);
-            }
-          }
-        )
-        .addPersistentLevel( //level2
-          Paths.get("level2"),
-          //todo provide simpler API for Java
-          CollectionConverters.asScala(Arrays.asList(new Dir(Paths.get("level2-1"), 1), new Dir(Paths.get("level2-3"), 1))).toList(),
-          true,
-          4000000, //4.mb
-          SortedKeyIndex.enableJava(
-            new PrefixCompression.Disable(true),
-            true,
-            ioAction -> new IOStrategy.SynchronisedIO(true),
-            info -> Collections.emptyList()
-          ),
-          RandomKeyIndex.enableJava(
-            1,
-            5,
-            2,
-            IndexFormat.Reference$.MODULE$,
-            RandomKeyIndex.RequiredSpace::requiredSpace,
-            ioAction -> new IOStrategy.SynchronisedIO(true),
-            info -> Collections.emptyList()
-          ),
-          BinarySearchIndex.fullIndexJava(
-            10,
-            ioAction -> new IOStrategy.SynchronisedIO(true),
-            IndexFormat.CopyKey$.MODULE$,
-            true,
-            info -> Collections.emptyList()
-          ),
-          MightContainIndex.enableJava(
-            0.01,
-            optimalMaxProbe -> 1,
-            10,
-            ioAction -> new IOStrategy.SynchronisedIO(true),
-            info -> Collections.emptyList()
-          ),
-          ValuesConfig.createJava(
-            true,
-            true,
-            ioAction -> new IOStrategy.SynchronisedIO(true),
-            info -> Collections.emptyList()
-          ),
-          SegmentConfig.createJava(
-            true,
-            true,
-            true,
-            MMAP.disabled(),
-            4000000,
-            100000,
-            ioStrategy -> {
-              if (ioStrategy.isOpenResource()) {
-                return new IOStrategy.SynchronisedIO(true);
-              } else if (ioStrategy.isReadDataOverview()) {
-                return new IOStrategy.SynchronisedIO(true);
-              } else {
-                IOAction.DataAction action = (IOAction.DataAction) ioStrategy;
-                return new IOStrategy.SynchronisedIO(action.isCompressed());
-              }
-            },
-            info ->
-              Arrays.asList(
-                Compression.lz4Pair(
-                  new Pair(LZ4Instance.fastestJavaInstance(), new LZ4Compressor.Fast(20.0)),
-                  new Pair(LZ4Instance.fastestJavaInstance(), LZ4Decompressor.fastDecompressor())
-                ),
-                new Compression.Snappy(20.0),
-                Compression.noneCompression()
-              )
-          ),
-          CompactionExecutionContext.Shared$.MODULE$,
-          (LevelMeter levelMeter) -> {
-            FiniteDuration delay = new FiniteDuration(5 - levelMeter.segmentsCount(), TimeUnit.SECONDS);
-            int batch = Math.min(levelMeter.segmentsCount(), 5);
-            return new Throttle(delay, batch);
-          }
-        )
-        .addTrashLevel(); //level3
+        .build()
+        .withMemoryLevel1(memoryLevel) //LEVEL 1
+        .withPersistentLevel(persistentLevel) //level2
+        .withTrashLevel(); //level3
   }
 }
